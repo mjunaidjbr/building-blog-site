e76684ec8ab57276700102df96a763abf45bff92
Muhammad Junaid Jabbar <raojunaidjabbar1999@gmail.com>
2024-10-26 07:54:21 +0500

    comment section add to posts

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
@@ -7,3 +8,9 @@ class EmailPostForm(forms.Form):
         required=False,
         widget=forms.Textarea
     )
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
