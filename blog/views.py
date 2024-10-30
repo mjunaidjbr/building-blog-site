c07c966b995e675803aecde453cbced1c55761a6
Muhammad Junaid Jabbar <raojunaidjabbar1999@gmail.com>
2024-10-30 15:50:02 +0500

    updated blog site files

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .forms import CommentForm, EmailPostForm
 from .models import Post
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
@@ -58,6 +59,14 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+     # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
 
     return render(
         request,
@@ -65,7 +74,8 @@ def post_detail(request, year, month, day, post):
         {
             'post': post,
             'comments': comments,
-            'form': form
+            'form': form,
+            'similar_posts': similar_posts
         }
     )
 
