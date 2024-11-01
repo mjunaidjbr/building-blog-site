4d1f971f1ce34f63dc2ee6ebe3d5ed080df18f33
Muhammad Junaid Jabbar <raojunaidjabbar1999@gmail.com>
2024-11-01 11:34:48 +0500

    full text search added

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,12 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.views.generic import ListView
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Post
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
@@ -152,5 +153,29 @@ def post_comment(request, post_id):
             'post': post,
             'form': form,
             'comment': comment
+        }
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    search=SearchVector('title', 'body'),
+                ).filter(search=query)
+            )
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
         }
     )
